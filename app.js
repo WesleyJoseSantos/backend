715012cecfe94c836fd9937b20c8012f84d20ef7
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-08-25 12:24:00 -0300

    feat: implementation

--- app.js
+++ app.js
@@ -1,29 +1,79 @@
 const express = require('express')
+const dotenv = require('dotenv')
+var bodyParser = require('body-parser')
 const mysql = require('mysql2');
 
 const app = express()
-const port = 3014
+dotenv.config({ path: `.env.${process.env.NODE_ENV || 'local'}` })
+
+const port = process.env.PORT
 const connection = mysql.createConnection({
-  host: 'aulascefet.c8tuthxylqic.sa-east-1.rds.amazonaws.com',
-  user: 'aluno',
-  password: 'alunoc3f3t',
-  database: 'aulas_web'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
 });
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Backend Wesley José Santos Rodando...')
 })
 
-app.get('/cliente', (req, res) => {
+app.post('/cliente', (req, res) => {
+  var user = req.body
+  var sql = `insert into 
+    cliente(
+      nome, 
+      sobrenome, 
+      email, 
+      salario, 
+      data_cadastro)
+    values(
+      "${user.nome}", 
+      "${user.sobrenome}", 
+      "${user.email}" 
+      "${user.salario}", 
+      "${(new Date()).toISOString()}"
+    )`
   connection.query(
-    'SELECT * FROM `cliente`',
+    sql, function (err, results, fields) {
+      if(err) console.error(err)
+      res.send(results)
+    }
+  )
+});
+
+app.delete('/cliente/:id', (req, res) => {
+  var sql = 'select * from cliente';
+  res.send(req.body);
+});
+
+app.get('/cliente', (req, res) => {
+  var sql = 'select * from cliente';
+  connection.query(
+    sql,
     function (err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      console.log(fields); // fields contains extra meta data about results, if available
-      res.send(JSON.stringify(results))
+      if(err) console.error(err)
+      res.send(results)
     }
   );
 })
+
+app.get('/cliente/:id', (req, res) => {
+  var id = req.params.id
+  var sql = `select * from cliente where id_cliente = ${id}`;
+  connection.query(
+    sql, (err, results, fields) => {
+      if(err) console.error(err)
+      res.send(results)
+    }
+  )
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 Porta: 3000 + número da chamada
 npm install pm2 -g
+url:https://github.com/WesleyJoseSantos/backend
+ip: 54.232.
