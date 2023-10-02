bf3dfa00070d68f366212158f62088ebdd94d2e9
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-10-02 17:32:01 -0300

    fea: cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const formData = require('express-form-data')
 const env = require('./env.js')
 const app = express()
+const cors = require('cors')
 
 const cliente = require('./routes/cliente.js')
 const fornecedor = require('./routes/fornecedor.js')
@@ -15,12 +16,23 @@ app.use(bodyParser.urlencoded({
 app.use(formData.parse())
 app.use(bodyParser.json())
 
+corsOpt = {
+  origin: 'http://127.0.0.1:5500',
+  optionsSuccessStatus: 200,
+}
+app.use(cors(corsOpt))
+
 app.use('/', cliente)
 app.use('/', fornecedor)
+app.use(express.static('uploads'))
 
 app.get('/', (req, res) => {
-  res.send(`Backend Wesley José Santos Rodando... (v${package.version}-${env.ENV})`)
-})
+  res.send({
+    author: package.author,
+    env: env.ENV,
+    version: package.version
+  })
+})  
 
 app.listen(env.PORT, () => {
   console.log(`Backend listening on port ${env.PORT}`)
